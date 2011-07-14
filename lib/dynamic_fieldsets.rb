8c0cf0479b77a3ccf08edc12813a89897b90dfa9
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-07-14 15:54:33 -0400

    Initial commit

---
+++ lib/dynamic_fieldsets.rb
@@ -0,0 +1,2 @@
+module DynamicFieldsets
+end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'ddd80d41cc58fb3fe2f20853876214dbb1986e330411c390dd6899e9f81831c2bee95768786031c189719d3dfe60eab6be44ed22552478d8284b7f86c4b14246'
