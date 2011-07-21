5e85758811db8fda56e4ea0fcf40a714c3495855
Scott <scott@cloudspace.com>
2011-07-20 23:04:44 -0400

    Fixing the field_option belongs_to to use symbol

--- app/models/dynamic_fieldsets/field_option.rb
+++ app/models/dynamic_fieldsets/field_option.rb
@@ -6,7 +6,7 @@ module DynamicFieldsets
   # @authors Scott Sampson, Jeremiah Hemphill, Ethan Pemble
   class FieldOption < ActiveRecord::Base
     #relations
-    belongs_to field
+    belongs_to :field
     
     #validations
     validates_presence_of :label
