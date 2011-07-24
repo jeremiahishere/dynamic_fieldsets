243861ee355284f3152f962b467a5edf832e062d
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-07-24 17:32:30 -0400

    Setup railtie and stubbed out methods in dynamic fieldsets model mixin.  Tests next.

---
+++ lib/dynamic_fieldsets/dynamic_fieldsets_in_model.rb
@@ -0,0 +1,54 @@
+module DynamicFieldsets
+  module DynamicFieldsetsInModel
+    def self.include(base)
+      base.extend(ClassMethods)
+    end
+
+    module ClassMethods
+      def acts_as_dynamic_fieldset(*args)
+        mattr_accessor :dynamic_fieldsets unless self.respond_to?(:dynamic_fieldsets)
+        args.each do |arg|
+          self.dynamic_fieldsets[arg[:name]] = arg
+        end
+
+        include DynamicFieldsets::DynamicFieldsetsInModel::InstanceMethods
+      end
+    end
+
+    module InstanceMethods
+
+      def method_missing
+        # if matches a form_name
+        #   get the form associator
+        # if matchs form_name_fieldset
+        #   get the fieldset
+        # call super
+      end
+
+      def respond_to?
+        # if it matches form_name or form_name_fieldset
+        #   return true
+        # call super
+      end
+
+      # method matches fingerprint_form
+      # want the fieldset associator object for that specified fieldset associator
+      def match_fieldset_associator?(sym, args)
+      end
+
+      # method matches fingerprint_form_fieldset
+      # want the fieldset object for that specified fieldset associator
+      def match_fieldset?(sym, args)
+      end
+
+      # returns the fieldset associator object for the named fieldset assocation
+      def fieldset_associator(sym)
+      end
+
+      # returns the fieldset object for the specified named fieldset association
+      def fieldset(sym)
+      end
+
+    end
+  end
+end
---
+++ lib/dynamic_fieldsets/railtie.rb
@@ -0,0 +1,13 @@
+require 'rails'
+
+# no configuration yet
+# require 'dynamic_fieldsets/config'
+
+module DynamicFieldsets
+  class Railtie < ::Rails::Railtie
+    initializer 'dynamic_fieldsets' do |app|
+      require 'dynamic_fieldsets/dynamic_fieldsets_in_model'
+      ActiveRecord::Base.send :include, DynamicFieldsets::DynamicFieldsetsInModel
+    end
+  end
+end
