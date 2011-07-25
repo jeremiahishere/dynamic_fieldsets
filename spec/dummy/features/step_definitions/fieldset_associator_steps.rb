79c281191dcb97e2ea6ab84e3e6c648c6cc9dc69
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-07-25 18:00:17 -0400

    Added step definition files.

---
+++ spec/dummy/features/step_definitions/fieldset_associator_steps.rb
@@ -0,0 +1,11 @@
+Given /^a fieldset exists$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^an information form exists$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^a fieldset associator exists$/ do
+  pending # express the regexp above with the code you wish you had
+end
