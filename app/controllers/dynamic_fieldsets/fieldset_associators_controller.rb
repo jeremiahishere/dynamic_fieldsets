194999099305c0983a10d90d1b9b865aa6502606
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-07-25 17:29:53 -0400

    Added a basic controller and views.

---
+++ app/controllers/dynamic_fieldsets/fieldset_associators_controller.rb
@@ -0,0 +1,24 @@
+# Controller for the FieldsetAssociator model
+# Allows users to view but not create or edit fieldset associators
+module DynamicFieldsets
+  class FieldsetAssociatorsController < ApplicationController
+    unloadable
+
+    # show all fieldset associators
+    def index
+      @fieldset_associators = DynamicFieldsets::FieldsetAssociator.all
+
+      respond_to do |format|
+        format.html
+      end
+    end
+
+    # show one fieldset associator
+    def show
+      @fieldset_associator = DynamicFieldsets::FieldsetAssociator.find_by_id(params[:id])
+      respond_to do |format|
+        format.html
+      end
+    end
+  end
+end
