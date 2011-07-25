b1ebeac93c5ab75e70428720013059553082adb9
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-07-25 10:53:52 -0400

    Still debugging mixin issues.

--- lib/dynamic_fieldsets/railtie.rb
+++ lib/dynamic_fieldsets/railtie.rb
@@ -6,8 +6,8 @@ require 'dynamic_fieldsets/config'
 module DynamicFieldsets
   class Railtie < ::Rails::Railtie
     initializer 'dynamic_fieldsets' do |app|
-      require 'dynamic_fieldsets/dynamic_fieldsets_in_model'
-      ActiveRecord::Base.send :include, DynamicFieldsets::DynamicFieldsetsInModel
+      require 'dynamic_fieldsets/multipart_form_in_model'
+      ActiveRecord::Base.send :include, DynamicFieldsets::MultipartFormInModel
     end
   end
 end
