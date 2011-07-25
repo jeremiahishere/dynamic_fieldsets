44a74c197f0e90cdb069e88ea7d7f6be4712493b
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-07-25 12:16:15 -0400

    Added comments to the model mixin.

--- lib/dynamic_fieldsets/dynamic_fieldsets_in_model.rb
+++ lib/dynamic_fieldsets/dynamic_fieldsets_in_model.rb
@@ -1,10 +1,26 @@
 module DynamicFieldsets
+  # Adds dynamic fieldsets to models.
+  # The configuration options here determine which fieldset is associated with the model
+  # and how it is identified.  The model knows which fieldset it is suppoed to be
+  # associated with and where the values for the fieldset are stored.
   module DynamicFieldsetsInModel
+
     def self.included(base)
       base.extend ClassMethods
     end
 
     module ClassMethods
+      # Adds a dynamic fieldset to a model
+      #
+      # Each set of options should be put inside of a hash with the unqiue name for the 
+      # fieldset as the key:
+      # :child_form => {options}
+      # Options:
+      # Fieldset: The unique name of the fieldset the class is associated with
+      # Multiple: Boolean value to allow multiple answer sets for the same fieldset in the class.
+      #   Deafult to false.  Not curently implemented (7-25-2011).
+      #
+      # @param [Hash] args A hash of arguments for the fieldsets.
       def acts_as_dynamic_fieldset(args)
         mattr_accessor :dynamic_fieldsets unless self.respond_to?(:dynamic_fieldsets)
         self.dynamic_fieldsets = {} unless self.dynamic_fieldsets.is_a?(Hash)
@@ -19,6 +35,12 @@ module DynamicFieldsets
 
     module InstanceMethods
 
+      # Matches methods that match named_fieldset and named_fieldset_fieldset
+      # Or calls super
+      #
+      # @param [Symbol] sym The name of the method
+      # @param [Array] args The arguments of the method
+      # @returns [?] The result of the found method or the result of super
       def method_missing(sym, *args)
         super(sym, *args)
         # if matches a form_name
@@ -28,6 +50,12 @@ module DynamicFieldsets
         # call super
       end
 
+      # Matches methods that match named_fieldset and named_fieldset_fieldset
+      # Or calls super
+      #
+      # @param [Symbol] sym The name of the method
+      # @param [Array] args The arguments of the method
+      # @returns [Boolean] True if the method is found or the result of super
       def respond_to?(sym, *args)
         # if it matches form_name or form_name_fieldset
         #   return true
@@ -35,21 +63,31 @@ module DynamicFieldsets
         super(sym, *args)
       end
 
-      # method matches fingerprint_form
-      # want the fieldset associator object for that specified fieldset associator
-      def match_fieldset_associator?(sym, args)
+      # Returns whether a method name matches a named fieldset
+      #
+      # @param [Symbol] The name of the method
+      # @param [Boolean] Whether the method name matches a named fieldset
+      def match_fieldset_associator?(sym)
       end
 
-      # method matches fingerprint_form_fieldset
-      # want the fieldset object for that specified fieldset associator
-      def match_fieldset?(sym, args)
+      # Returns whether a method name matches a named fieldset followed by '_fieldset'
+      #
+      # @param [Symbol] The name of the method
+      # @param [Boolean] Whether the method name matches a named fieldset
+      def match_fieldset?(sym)
       end
 
-      # returns the fieldset associator object for the named fieldset assocation
+      # Returns the fieldset associator object for the named fieldset
+      #
+      # @param [Symbol] The name of the name fieldset
+      # @param [FieldsetAssociator] The fieldset associator object for the named fieldset
       def fieldset_associator(sym)
       end
 
-      # returns the fieldset object for the specified named fieldset association
+      # Returns the fieldset object for the named fieldset
+      #
+      # @param [Symbol] The name of the named fieldset
+      # @param [Fieldset] The fieldset object for the named fieldset
       def fieldset(sym)
       end
 
