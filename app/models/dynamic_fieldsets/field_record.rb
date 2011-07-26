78823757a39aba46741fdb84f6872e6d682521e5
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-07-26 16:26:38 -0400

    Got spec tests working.

--- app/models/dynamic_fieldsets/field_record.rb
+++ app/models/dynamic_fieldsets/field_record.rb
@@ -1,4 +1,12 @@
+# add relationships to field and fieldset_associator
+#
+
 module DynamicFieldsets
   class FieldRecord < ActiveRecord::Base
+    belongs_to :field
+    belongs_to :fieldset_associator
+
+    validates_presence_of :field, :fieldset_associator
+    validates_exclusion_of :value, :in => [nil] 
   end
 end
