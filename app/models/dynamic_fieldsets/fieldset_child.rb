007881191ad07ad80f1f5cf19c44fa6e9d3632a3
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-08-01 19:45:05 -0400

    Setup the fieldset children model and spec tests.

--- app/models/dynamic_fieldsets/fieldset_child.rb
+++ app/models/dynamic_fieldsets/fieldset_child.rb
@@ -1,4 +1,19 @@
 module DynamicFieldsets
   class FieldsetChild < ActiveRecord::Base
+    belongs_to :child, :polymorphic => true
+    belongs_to :fieldset
+
+    validates_presence_of :fieldset_id
+    validates_presence_of :child_id
+    validates_presence_of :child_type
+    validates_presence_of :order_num
+    validate :no_duplicate_fields_in_fieldset_children
+
+    def no_duplicate_fields_in_fieldset_children
+      duplicate_children = FieldsetChild.where(:fieldset => self.fieldset, :child_id => self.child_id, :child_type => self.child_type).select { |child| child.id != self.id }
+      if duplicate_children > 0
+        self.errors.add(:child, "There is already a copy of this child in the fieldset.")
+      end
+    end
   end
 end
--- spec/models/fieldset_child_spec.rb
+++ spec/models/fieldset_child_spec.rb
@@ -2,17 +2,45 @@ require 'spec_helper'
 include DynamicFieldsets
 
 describe FieldsetChild do
-  #include FieldsetChildHelper
+  include FieldsetChildHelper
 
-  it "should respond to field"
-  it "should respond to order_num"
-  it "should respond to child"
+  it "should respond to fieldset" do
+    FieldsetChild.new.should respond_to :fieldset
+  end
+
+  it "should respond to child" do
+    FieldsetChild.new.should respond_to :child
+  end
+
+  it "should respond to order_num" do
+    FieldsetChild.new.should respond_to :order_num
+  end
 
   describe "validations" do
-    it "should require field"
-    it "should require child"
-    it "should require order number"
-    it "should only allow one field per fieldset"
+    before(:each) do
+      @fieldset_child = FieldsetChild.new
+    end
+    it "should be valid" do
+      @fieldset_child.attributes = valid_attributes
+      @fieldset_child.should be_valid?
+    end
+    
+    it "should require fieldset" do
+      @fieldset_child.should have(1).error_on(:fieldset_id)
+    end
+
+    it "should require child" do
+      @fieldset_child.should have(1).error_on(:child_id)
+      @fieldset_child.should have(1).error_on(:child_type)
+    end
+
+    it "should require order number" do
+      @fieldset_child.should have(1).error_on(:order_num)
+    end
+
+    it "should only allow one field per fieldset" do
+      pending "doing this one later"
+    end
   end
 
 end
