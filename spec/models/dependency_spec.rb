7e39c6404ab7a99788969ffb0e5b4b1d9e429e0b
John Carter <hex@cloudspace.com>
2011-08-08 12:11:53 -0400

    Finished preliminary spec testing - about to write actual validations

--- spec/models/dependency_spec.rb
+++ spec/models/dependency_spec.rb
@@ -2,16 +2,54 @@ require 'spec_helper'
 include DynamicFieldsets
 
 describe Dependency do
-  it "should respond to fieldset_child"
-  it "should respond to dependency_clauses"
+
+  it "should respond to fieldset_child" do
+    pending "hex is working on this"
+    Dependency.new.should respond_to :fieldset_child
+  end
+
+  it "should respond to dependency_clauses" do
+    pending "hex is working on this"
+    Dependency.new.should respond_to :dependency_clauses
+  end
 
   describe "validations" do
-    it "should be valid"
-    it "should have a field_id"
-    it "should have a value"
-    it "should have a relationship"
-    it "should have a dependency_clause_id"
-    it "should have a relationship limited to allowable values" 
+    before(:each) do
+      pending "hex is working on this"
+      @dependency = Dependency.new
+    end
+
+    it "should be valid" do
+      pending "hex is working on this"
+      @dependency.attributes = valid_attributes
+      @dependency.should be_valid
+    end
+
+    it "should have a field_id" do
+      pending "hex is working on this"
+      @dependency.should have(1).error_on(:field_id)
+    end
+
+    it "should have a value" do
+      pending "hex is working on this"
+      @dependency.shoud have(1).error_on(:value)
+    end
+
+    it "should have a relationship" do
+      pending "hex is working on this"
+      @dependency.should have(1).error_on(:relationship)
+    end
+
+    it "should have a dependency_clause_id" do
+      pending "hex is working on this"
+      @dependency.should have(1).error_on(:dependency_clause_id)
+    end
+
+    it "should have a relationship limited to allowable values" do
+      pending "hex is working on this"
+      @dependency.relationship = "invalid relationship"
+      @dependnecy.should have(1).error_on(:relationship)
+    end
   end
 
   describe "evaluate" do
@@ -54,18 +92,101 @@ describe Dependency do
   end
 
   describe "process_relationship" do
-
+    before(:each) do
+      pending "hex is working on this"
+      @dependency = Dependency.new
+      @dependency.value = "test string"
+    end
 
     it "should take a string as input" do
       pending "hex is working on this"
-      
+      @dependency.process_relationship(@input_value).should_not raise_error
     end
-    it "should call eql? if relationship is 'equals'"
-    it "should call !eql? if relationship is 'not equals'"
-    it "should call includes? if relationship is 'includes'"
-    it "should call !includes? if relationship is 'not includes'"
-    it "should call empty? if relationship is 'blank'"
-    it "should call !empty? if relationship is 'not blank'"
+
+    it "should return true if the value and input_value are equal and relatinship is equal" do
+      pending "hex is working on this"
+      @dependency.relationship = "equal"
+      input_value = "test string"
+      @dependency.process_relationship(input_value).should be_true
+    end
+
+    it "should return false if the value and input value are not equal and relationship is equal" do
+      pending "hex is working on this"
+      @dependency.relationship = "equal"
+      input_value = "not test string"
+      @dependency.process_relationship(input_value).should be_false
+    end
+
+    it "should return true if the value and input_value are not equal and relationship is not equal" do
+      pending "hex is working on this"
+      @dependency.relationship = "not equal"
+      input_value = "not test string"
+      @dependency.process_relationship(input_value).should be_true
+    end
+
+    it "should return false if the value and input_value are equal and relationship is not equal" do
+      pending "hex is working on this"
+      @dependency.relationship = "not equal"
+      input_value = "test string"
+      @dependency.process_relationship(input_value).should be_false
+    end
+
+    it "should return true if the value is within the input_value array and relationship is includes" do
+      pending "hex is working on this"
+      @dependency.relationship = "includes"
+      input_value = ["test_string"]
+      @dependency.process_relationship(input_value).should be_true 
+    end
+
+    it "should return false if the value is not within the input_value array and relationship is includes" do
+      pending "hex is working on this"
+      @dependency.relationship = "includes"
+      input_value = ["a"]
+      @dependency.process_relationship(input_value).should be_false
+    end
+
+    it "should return true if the value is not within the input_value array and relationship is not includes" do
+      pending "hex is working on this"
+      @dependency.relationship = "not includes"
+      input_value = ["a"]
+      @dependency.process_relationship(input_value).should be_true
+    end
+
+    it "should return false if the value is within the input_value array and relationship is not includes" do
+      pending "hex is working on this"
+      @dependency.relationship = "not includes"
+      input_value = ["test_string"]
+      @dependency.process_relationship(input_value).should be_false
+    end
+
+    it "should return true if the input_value is an empty string and relationship is blank" do
+      pending "hex is working on this"
+      @dependency.relationship = "blank"
+      input_value = ""
+      @dependency.process_relationship(input_value).should be_true
+    end
+
+    it "should return false if the input_value is not an empty string and relationship is blank" do
+      pending "hex is working on this"
+      @dependency.relationship = "blank"
+      input_value = "test string"
+      @dependency.process_relationship(input_value).should be_false
+    end
+
+    it "should return true if the input_value is not an empty string and relationship is not blank" do
+      pending "hex is working on this"
+      @dependency.relationship = "not blank"
+      input_value = "test string"
+      @dependency.process_relationship(input_value).should be_true
+    end
+
+    it "should return false if the input_value is not an empty string and relationship is not blank" do
+      pending "hex is working on this"
+      @dependency.relationship = "not blank"
+      input_value = ""
+      @dependency.process_relationship(input_value).should be_false
+    end
+
   end
 
 end
