58e4069b8399a9ce6fbc1f3ab9f2574206b5ebf8
John Carter <hex@cloudspace.com>
2011-08-09 11:31:52 -0400

    Added commenting to the dependency field model and adjusted the constant for relationship_types

--- app/models/dynamic_fieldsets/dependency.rb
+++ app/models/dynamic_fieldsets/dependency.rb
@@ -1,12 +1,11 @@
 module DynamicFieldsets
+
   # Logical atom for dealing with dependency clauses
   #
   # @author John "hex" Carter
-
-  $relationship_list = ["equals","not equals","includes","not includes","blank","not blank"]
-
   class Dependency < ActiveRecord::Base
 
+    RELATIONSHIP_LIST = ["equals","not equals","includes","not includes","blank","not blank"]
 
     # Relations    
 
@@ -17,12 +16,22 @@ module DynamicFieldsets
     validates_presence_of :fieldset_child_id
     validates_presence_of :value
     validates_presence_of :dependency_clause_id
-    validates_inclusion_of :relationship, :in => $relationship_list
+    validates_inclusion_of :relationship, :in => RELATIONSHIP_LIST
 
+    # Returns a full list of the options for relationship_list
+    #
+    # @params [None]
+    # @returns [Array] The array of the constant RELATIONSHIP_LIST
+    #
     def relationship_list
-      return $relationship_list
+      return RELATIONSHIP_LIST
     end
 
+    # Returns true or false based on whether the value pushed in matches the logical relationship between it and the dependency's value 
+    #
+    # @params [String] input_value - The value to be tested
+    # @returns [Boolean] Logical response to relationship between the dependency value and the pushed value
+    #
     def process_relationship(input_value)
       case self.relationship
       when "equals" 
@@ -42,6 +51,11 @@ module DynamicFieldsets
       end
     end
 
+    # Looks through the input_hash for the fieldset_child that matches the one belonging to this dependency and compares the values through process_relationship
+    #
+    # @params [Hash] input_hash - Key is the id of the fieldset_child to be tested, value is the input given by the user
+    # @returns [Boolean] returns the value of process_relationship unless the input doesn't contain the fieldset_child of the dependency, which also returns false 
+    #
     def evaluate(input_hash)
       input_value = input_hash[self.fieldset_child_id]
       unless input_value.nil?
