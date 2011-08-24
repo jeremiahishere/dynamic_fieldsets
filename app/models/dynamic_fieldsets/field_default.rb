4856a7183532a3efbca82390327a3f46ed55a53f
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-08-24 12:01:58 -0400

    Added support to convert default values into field options id if the field has a field option type.  Added spec tests for the field model.  Updated the view, partial, and helper to make sure the user never sees an id even though they are happening behind the scenes.

--- app/models/dynamic_fieldsets/field_default.rb
+++ app/models/dynamic_fieldsets/field_default.rb
@@ -10,5 +10,31 @@ module DynamicFieldsets
     
     #validations
     validates_presence_of :value
+
+    before_save :convert_option_name_to_id
+    
+    # When the field type is an option type, the saved value should be converted into an id
+    # This needs to happen because the value field normally stores a string but sometimes stores a field option id
+    #
+    # In some cases, the field_option instance is not set before this, no idea what happens then
+    #
+    # http://www.youtube.com/watch?v=BeP6CpUnfc0 
+    def convert_option_name_to_id
+      if Field.option_field_types.include?(self.field.field_type)
+        option = FieldOption.find_by_name(self.value)
+        self.value = option.id unless option.nil?
+      end
+    end
+
+    # @return [String] Either the value or the name of the field option reference by the value
+    def pretty_value
+      if !self.field.nil? && Field.option_field_types.include?(self.field.field_type)
+        option = FieldOption.find_by_id(self.value)
+        if !option.nil?
+          return option.name
+        end
+      end
+      return self.value
+    end
   end
 end
--- spec/models/field_default_spec.rb
+++ spec/models/field_default_spec.rb
@@ -23,4 +23,53 @@ describe FieldDefault do
       @field_default.should have(1).error_on(:value)
     end
   end  
+
+  # note that these are saving because I am testing a before save
+  # instead of testing the method directly
+  describe "convert_option_name_to_id method" do
+    before(:each) do
+      @field = Field.new(:field_type => "select")
+      @field.stub!(:id).and_return(1)
+      @field_option = FieldOption.new(:name => "test value")
+      @field_option.stub!(:id).and_return(2)
+      FieldOption.stub!(:find_by_name).and_return(@field_option)
+      @default = FieldDefault.new(:field => @field, :value => "test value")
+    end
+
+    it "should convert the value to a field option id if the field's type is an option type" do
+      @default.save
+      @default.value.should == @field_option.id
+    end
+
+    it "should retain it's value if the field's type is not an option type" do
+      @field.field_type = "textfield"
+      @default.save
+      @default.value.should_not == @field_option.id
+    end
+  end
+
+  describe "pretty_value method" do
+    before(:each) do
+      @field = Field.new(:field_type => "select")
+      @field.stub!(:id).and_return(1)
+      @field_option = FieldOption.new(:name => "test value")
+      @field_option.stub!(:id).and_return(2)
+      FieldOption.stub!(:find_by_name).and_return(@field_option)
+      @default = FieldDefault.new(:field => @field, :value => "test value")
+    end
+
+    it "should return the value if the field is not an option type" do
+      @field.field_type = "textfield"
+      @default.pretty_value.should == @default.value
+    end
+
+    it "should return the field option name if the field is an option type" do
+      @default.pretty_value.should == @field_option.name
+    end
+      
+    it "should return the value if the field is not set" do
+      @default.field = nil
+      @default.pretty_value.should == @default.value
+    end
+  end
 end
