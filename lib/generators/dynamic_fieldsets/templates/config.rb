96d4194d36db950d3de31aa52e5114f942b417dc
Jeremiah Hemphill <jeremiah@cloudspace.com>
2012-03-01 11:12:34 -0500

    Fixed a typo in the generated config.

--- lib/generators/dynamic_fieldsets/templates/config.rb
+++ lib/generators/dynamic_fieldsets/templates/config.rb
@@ -3,7 +3,7 @@ DynamicFieldsets.configure do |config|
   # can cause the single table inheritance to fail.  This is due to the object
   # space getting reset after every request.  In production and test environemnts, 
   # this information will not be used because the object space will be complete.
-  # config.available_field_types = [ "DynamicFieldsets::CheckboxField", "DynamicFieldsets::"DateField", "DynamicFieldsets::"DatetimeField", "DynamicFieldsets::InstructionField", "DynamicFieldsets::MultipleSelectField", "DynamicFieldsets::RadioField", "DynamicFieldsets::SelectField", "DynamicFieldsets::TextField", "DynamicFieldsets::TextareaField" ]
+  # config.available_field_types = [ "DynamicFieldsets::CheckboxField", "DynamicFieldsets::DateField", "DynamicFieldsets::DatetimeField", "DynamicFieldsets::InstructionField", "DynamicFieldsets::MultipleSelectField", "DynamicFieldsets::RadioField", "DynamicFieldsets::SelectField", "DynamicFieldsets::TextField", "DynamicFieldsets::TextareaField" ]
 
   # This should correspond to the beginning of the id for the fields for on the form
   # For example: fsa-8
